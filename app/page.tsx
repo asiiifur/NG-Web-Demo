9f6f78fbe8ba643402693aa1fd699e77a6a4deee
ASIFUR RAHAMAN <asif121527@gmail.com>
2025-08-21 12:59:57 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { ServicesSection } from "@/components/services-section"
 import { AboutSection } from "@/components/about-section"
 import { StatsSection } from "@/components/stats-section"
 import { Footer } from "@/components/footer"
-import { ImageManager } from "@/components/image-manager"
 
 export default function Home() {
   return (
@@ -17,7 +16,6 @@ export default function Home() {
         <StatsSection />
       </main>
       <Footer />
-      <ImageManager />
     </>
   )
 }
